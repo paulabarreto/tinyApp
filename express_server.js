33d73813c5cd8f4708485093c27ce89619446703
Paula Barreto <paulavilaca@gmail.com>
2018-09-04 16:30:15 -0400

    views and server

---
+++ express_server.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var app = express();
+var PORT = 8080;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  console.log(req.params.id);
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+function generateRandomString() {
+  let result = "";
+  for(i = 0; i < 6; i++){
+    result += Math.random().toString(36).replace('0.', '').slice(1, 2);
+  }
+  return result;
+}
